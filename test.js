2bc06bea1ee1def4a3ab53b85c2a4eac8b488ad2
Bas de Ruijter <me@basdupre.com>
2020-08-20 10:41:33 +0200

    test

--- test.js
+++ test.js
@@ -3,6 +3,5 @@ var addon = require('bindings')('virplugs-audioengine');
 console.dir(addon, { depth: null });
 //console.dir(addon.getDeviceInfo(), { depth: null });
 
-console.dir(addon.readAudioFileInfo("d:\\drumLoop.wav"));
-console.dir(addon.readAudioFileWaveform("d:\\drumLoop.wav", 128));
-console.dir(addon.readAudioFileWaveform("d:\\drumLoop.wav", 512)[0].length);
+console.dir(addon.readAudioFileInfo("drumLoop.wav"));
+console.dir(addon.readAudioFileWaveform("drumLoop.wav", 128));
